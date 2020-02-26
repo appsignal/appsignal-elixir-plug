84831b1e5ff5d145bfac76c1186ad4239fdc5296
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-26 15:57:09 +0100

    Remove generated code from Appsignal.Plug(Test)

--- lib/appsignal_plug.ex
+++ lib/appsignal_plug.ex
@@ -1,18 +1,2 @@
 defmodule Appsignal.Plug do
-  @moduledoc """
-  Documentation for Appsignal.Plug.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Appsignal.Plug.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/appsignal_plug_test.exs
+++ test/appsignal_plug_test.exs
@@ -1,8 +1,3 @@
 defmodule Appsignal.PlugTest do
   use ExUnit.Case
-  doctest Appsignal.Plug
-
-  test "greets the world" do
-    assert Appsignal.Plug.hello() == :world
-  end
 end
