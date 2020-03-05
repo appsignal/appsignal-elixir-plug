cad64e9d0bb51aff68e08d077d43f9da34f7b870
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-05 16:11:18 +0100

    Appsignal.Plug creates a Span for every request

--- lib/appsignal_plug.ex
+++ lib/appsignal_plug.ex
@@ -1,2 +1,18 @@
 defmodule Appsignal.Plug do
+  @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
+
+  defmacro __using__(_) do
+    quote do
+      plug(Appsignal.Plug)
+    end
+  end
+
+  def init(opts) do
+    opts
+  end
+
+  def call(conn, _opts) do
+    @tracer.create_span("")
+    conn
+  end
 end
--- test/appsignal_plug_test.exs
+++ test/appsignal_plug_test.exs
@@ -1,3 +1,30 @@
+defmodule PlugWithAppsignal do
+  use Plug.Router
+  use Appsignal.Plug
+
+  plug(:match)
+  plug(:dispatch)
+
+  get "/" do
+    send_resp(conn, 200, "Welcome")
+  end
+end
+
 defmodule Appsignal.PlugTest do
   use ExUnit.Case
+  use Plug.Test
+  alias Appsignal.Test.Tracer
+
+  setup do
+    Tracer.start_link()
+    :ok
+  end
+
+  test "creates a root span" do
+    :get
+    |> conn("/")
+    |> PlugWithAppsignal.call([])
+
+    assert Tracer.get(:create_span) == [{""}]
+  end
 end
