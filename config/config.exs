986860a0f8a1a3a9c3515b89d87be9dc42bda531
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-17 16:38:35 +0100

    Add test configuration to config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,10 @@ use Mix.Config
 if Mix.env() == :test do
   config :appsignal, appsignal_tracer: Appsignal.Test.Tracer
   config :appsignal, appsignal_span: Appsignal.Test.Span
+
+  config :appsignal, :config,
+    push_api_key: "00000000-0000-0000-0000-000000000000",
+    name: "appsignal-plug",
+    env: "test",
+    active: true
 end
