56a6085c6eeb389ee04f0ce4cfb98bb149834eed
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-07-07 12:39:05 +0200

    Switch to :appsignal ~> 2.0.0-alpha.6

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Appsignal.Plug.MixProject do
   defp deps do
     [
       {:plug, ">= 1.1.0"},
-      {:appsignal, github: "appsignal/appsignal-elixir", branch: "tracing"},
+      {:appsignal, "~> 2.0.0-alpha.6"},
       {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false}
