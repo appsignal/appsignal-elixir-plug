01feccd28165b01e643d211bcc28883c913d6c06
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-17 10:55:20 +0100

    Disable Credo.Check.Warning.ApplicationConfigInModuleAttribute
    We're aware using configuration in module attributes are evaluated during
    compile time. We use these for switching out modules with mocks (verb) in the
    test environment only. We can't switch to using `Application.compile_env/2`
    right now, as we still support Elixir 1.9.

--- .credo.exs
+++ .credo.exs
@@ -149,6 +149,7 @@
         {Credo.Check.Warning.UnusedStringOperation, []},
         {Credo.Check.Warning.UnusedTupleOperation, []},
         {Credo.Check.Warning.UnsafeExec, []},
+	{Credo.Check.Warning.ApplicationConfigInModuleAttribute, false},
 
         #
         # Checks scheduled for next check update (opt-in for now, just replace `false` with `[]`)
