efae198283e1d7c37d6c165a99631c45cbef723f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-17 10:03:32 +0100

    Release 2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Appsignal.Plug.MixProject do
   def project do
     [
       app: :appsignal_plug,
-      version: "2.0.0-beta.4",
+      version: "2.0.0",
       description:
         "AppSignal's Plug instrumentation instruments calls to Plug applications to gain performance insights and error reporting",
       package: %{
