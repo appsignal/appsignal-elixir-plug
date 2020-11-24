a184eece797279dbce365afeaf5c63c08ba0108c
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-24 11:16:53 +0100

    Release 2.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 2.0.1
+Explicitly ignore return from span functions in Appsignal.Plug. PR #4
+
 # 2.0.0
 * Initial release, extracted from appsignal-elixir 🎉
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Appsignal.Plug.MixProject do
   def project do
     [
       app: :appsignal_plug,
-      version: "2.0.0",
+      version: "2.0.1",
       description:
         "AppSignal's Plug instrumentation instruments calls to Plug applications to gain performance insights and error reporting",
       package: %{
