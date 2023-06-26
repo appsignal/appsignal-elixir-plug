e1c8f87ba16dfd3a50b14cb7ecd37c9824a9b5e5
Noemi <45180344+unflxw@users.noreply.github.com>
2023-06-26 19:18:29 +0200

    Do not use `compile_env` wrapper
    The `Appsignal.Utils.compile_env` wrapper has been removed, as it
    only existed to special case older Elixir versions that are no
    longer supported. Use `Application.compile_env` directly.

--- lib/appsignal_plug.ex
+++ lib/appsignal_plug.ex
@@ -1,6 +1,6 @@
 defmodule Appsignal.Plug do
-  require Appsignal.Utils
-  @span Appsignal.Utils.compile_env(:appsignal, :appsignal_span, Appsignal.Span)
+  require Application
+  @span Application.compile_env(:appsignal, :appsignal_span, Appsignal.Span)
 
   @moduledoc """
   AppSignal's Plug instrumentation instruments calls to Plug applications to
@@ -28,11 +28,11 @@ defmodule Appsignal.Plug do
   defmacro __using__(_) do
     quote do
       require Logger
-      require Appsignal.Utils
+      require Application
       Appsignal.IntegrationLogger.debug("AppSignal.Plug attached to #{__MODULE__}")
 
-      @tracer Appsignal.Utils.compile_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
-      @span Appsignal.Utils.compile_env(:appsignal, :appsignal_span, Appsignal.Span)
+      @tracer Application.compile_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
+      @span Application.compile_env(:appsignal, :appsignal_span, Appsignal.Span)
 
       use Plug.ErrorHandler
 
